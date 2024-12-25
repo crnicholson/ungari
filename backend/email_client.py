94c839a1cc3a44e8499316a2b508a83cbf7b46bc
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-12-25 22:34:34 +0000

    More email changes

--- backend/email_client.py
+++ backend/email_client.py
@@ -23,18 +23,18 @@ msg["To"] = target_email
 msg["Subject"] = subject
 msg.attach(MIMEText(body, "plain"))
 
-# try:
-s = smtplib.SMTP("smtp.gmail.com", 587)
-s.starttls()
-s.login(SENDER_EMAIL, SENDER_PASSWORD)
-message = "Testing"
-s.sendmail(
-    SENDER_EMAIL,
-    target_email,
-    msg.as_string(),
-)
-print(f"\nEmail sent successfully to {target_email} with message:\n{body}.")
-# except Exception as e:
-#     print(f"Failed to send email: {e}")
-# finally:
-s.quit()
+try:
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+    s.starttls()
+    s.login(SENDER_EMAIL, SENDER_PASSWORD)
+    message = "Testing"
+    s.sendmail(
+        SENDER_EMAIL,
+        target_email,
+        msg.as_string(),
+    )
+    print(f"\nEmail sent successfully to {target_email} with message:\n{body}.")
+except Exception as e:
+    print(f"Failed to send email: {e}")
+finally:
+    s.quit()
